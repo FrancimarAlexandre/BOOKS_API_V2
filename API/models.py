ebbc95853c3cd532a40972a3ad79fada5d884ea8
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-06-20 18:18:19 -0300

    criando os models

--- API/models.py
+++ API/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+    DataNasc = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Book(models.Model):
+    autor = models.ForeignKey(Autor,on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    DataPub = models.DateField()
+    DataCad = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Livro {self.titulo} do autor {self.autor}'
